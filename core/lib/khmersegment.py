3aec79d1a641fc4c27cba59e21854fd735e0ba84
sokpheng <sokphengcheang@gmail.com>
2018-12-15 13:23:48 +0700

    add khmer language configuration

--- core/lib/khmersegment.py
+++ core/lib/khmersegment.py
@@ -4,14 +4,16 @@ import time
 
 class KhmerSegment:
 	def __init__(self):
-		# self.PATH = os.environ['KM_PATH']
-    self.PATH = 'D:\km-5tag-seg-1.0\km-5tag-seg-1.0'
+		self.PATH = "D:/km-5tag-seg-1.0"
+    	# self.PATH = 'D:\km-5tag-seg-1.0\km-5tag-seg-1.0'
 
 	def add_to_file(self, text):
 		seconds = str(int(time.time() * 1000000))
 		file_name = seconds + '.txt'
 		file = self.PATH + '/sample/' + file_name
-		f = open(file, 'w')
+		print('file', file)
+		# f = open(file, 'w')
+		f = open(file, 'w', encoding="utf-8", errors="surrogateescape")
 		f.write(text)
 		f.close()
 		return file_name
--- core/lib/unicodesplit.py
+++ core/lib/unicodesplit.py
@@ -11,14 +11,16 @@ class UnicodeSplit:
   def unicode_split(self,text):
     segment = KhmerSegment()
     file_name = segment.add_to_file(text)
-
     # Run command to convert the sentence into separated words
-    command = 'cd ' + segment.PATH + ' && ./km-5tag-seg-test.sh model/km-5tag-seg-model sample/' + file_name + ' sample-out/'
+    if os.name == 'nt':
+      command = 'cd ' + segment.PATH + ' && km-5tag-seg-test.sh model/km-5tag-seg-model sample/' + file_name + ' sampleout/'
+    else:
+      command = 'cd ' + segment.PATH + ' && ./km-5tag-seg-test.sh model/km-5tag-seg-model sample/' + file_name + ' sampleout/'
     KhmerSegment.run_command(command)
 
     input_file = segment.PATH + '/sample/' + file_name
-    output_file_w = segment.PATH + '/sample-out/' + file_name + '.w'
-    output_file_c = segment.PATH + '/sample-out/' + file_name + '.c'
+    output_file_w = segment.PATH + '/sampleout/' + file_name + '.w'
+    output_file_c = segment.PATH + '/sampleout/' + file_name + '.c'
 
     # Read temporary file content
     result = KhmerSegment.read_file(output_file_w).split(' ')
